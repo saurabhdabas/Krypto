cf53e7c7c73df954101a34f748796063527bd6db
Saurabh Dabas <developerdabas@gmail.com>
2022-04-15 08:56:45 +0530

    Add a test query

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = (db) => {
+  // all routes will go here 
+  router.get('/', (req, res) => {
+    const command = "SELECT * FROM users";
+    db.query(command).then(data => {
+      res.json(data.rows);
+    })
+  });
 
-module.exports = router;
+  return router;
+}
+
