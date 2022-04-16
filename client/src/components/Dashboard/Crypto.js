974c9cb64b4d5d19edce4252017289856cd12da3
Saurabh Dabas <developerdabas@gmail.com>
2022-04-16 20:39:41 +0530

    Add A link to crypto image

--- client/src/components/Dashboard/Crypto.js
+++ client/src/components/Dashboard/Crypto.js
@@ -1,17 +1,20 @@
 import React from 'react'
-
-const TrendingCrypto = (props) => {
+import { Link } from 'react-router-dom';
+const Crypto = (props) => {
+  console.log("props:",props);
   return (
-    <li>
-      <img src={props.image} alt={props.name}></img>
-      <div>
-        <span>{props.name}</span>
-        <span>{props.price_change_percentage_24h}</span>
-      </div>
-      <div>{props.current_price}</div>
-      <div>{props.last_updated}</div>
-    </li>
+      <li>
+        <Link to={`/crypto/${props.name.toLowerCase()}`}>
+          <img src={props.image} alt={props.name}></img>
+        </Link>
+        <div>
+          <span>{props.name}</span>
+          <span>{props.price_change_percentage_24h}</span>
+        </div>
+        <div>{props.current_price}</div>
+        <div>{props.last_updated}</div>
+      </li>
   )
 }
 
-export default TrendingCrypto
+export default Crypto
