dd70cae6c9105581fb1889874a434900586a0d79
Saurabh Dabas <developerdabas@gmail.com>
2022-04-16 08:36:32 +0530

    Add SideBar Component and Add a WatchList route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
+import SideBarList from './components/SideBar/SideBarList';
+import WatchList from './components/WatchList';
 
 function App() {
   return (
     <>
+      <SideBarList/>
       <Routes>
-        <Route path="/dashboard/" element={<Dashboard/>}/>  
+        <Route path="/dashboard/" element={<Dashboard/>}/> 
+        <Route path="/watchlist/" element={<WatchList/>}/>
       </Routes>
     </>
 
