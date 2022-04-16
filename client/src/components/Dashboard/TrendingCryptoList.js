4d087ecead7906218996215ed744b58f20568c15
Saurabh Dabas <developerdabas@gmail.com>
2022-04-16 08:14:11 +0530

    Add Trending Crypto List component

---
+++ client/src/components/Dashboard/TrendingCryptoList.js
@@ -0,0 +1,26 @@
+import {React} from 'react';
+
+import TrendingCrypto from './TrendingCrypto';
+
+const TrendingCryptoList = (props) => {
+  console.log("props:",props.data);
+  const TrendingCryptoList = props.data.map((crypto)=>{
+    return (
+      <TrendingCrypto
+      key = {crypto.id}
+      image={crypto.image}
+      name={crypto.name}
+      price_change_percentage_24h={crypto.price_change_percentage_24h}
+      current_price={crypto.current_price}
+      last_updated={crypto.last_updated}
+      />
+    );
+  });
+  return (
+    <div>
+      {TrendingCryptoList}
+    </div>
+  )
+}
+
+export default TrendingCryptoList
