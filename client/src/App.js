4d62662b087a8aad4c50e7d5f39228cea00a47f6
Saurabh Dabas <developerdabas@gmail.com>
2022-04-16 20:38:54 +0530

    specify a route for single crypto component

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import SideBarList from './components/SideBar/SideBarList';
 import WatchList from './components/WatchList';
+import SingleCrypto from './components/Dashboard/SingleCrypto';
 
 function App() {
   return (
     <>
       <SideBarList/>
       <Routes>
-        <Route path="/dashboard/" element={<Dashboard/>}/> 
-        <Route path="/watchlist/" element={<WatchList/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/> 
+        <Route path="/watchlist" element={<WatchList/>}/>
+        <Route path="/crypto/:id" element={<SingleCrypto/>}/>
       </Routes>
     </>
 
