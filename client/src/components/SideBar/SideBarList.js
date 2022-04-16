53539100f3d7b05e9f70845fa10b38b613b99467
Saurabh Dabas <developerdabas@gmail.com>
2022-04-16 08:34:52 +0530

    Add SideBar Component

---
+++ client/src/components/SideBar/SideBarList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const sideBarList = () => {
+  return (
+    <div>
+      <div> 
+        <img src="https://pickaface.net/gallery/avatar/20160625_050020_889_FAKE.png" alt="avatar"/>
+        <p>Saurabh</p>
+      </div>
+      <ul>
+        <li>
+        <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+        <Link to="/watchlist">Watchlist</Link>
+        </li>
+        <li>
+        <Link to="/news">News</Link>
+        </li>
+        <li>
+        <Link to="/chatrooms">Chat</Link>
+        </li>
+        <li>
+        <Link to="/cryptotools">Tools</Link>
+        </li>
+        <li>
+        <Link to="/logout">Logout</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default sideBarList
+
+
