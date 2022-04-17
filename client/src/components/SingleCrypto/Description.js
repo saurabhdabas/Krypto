365dfe75f5ca6c5873c14edf5bb64aa20844d781
Saurabh Dabas <developerdabas@gmail.com>
2022-04-18 03:28:27 +0530

    Add conditional rendering for crypto metrics

--- client/src/components/SingleCrypto/Description.js
+++ client/src/components/SingleCrypto/Description.js
@@ -4,15 +4,15 @@ const Description = (props) => {
   
   return (
     <div>
-      <img src={props.description.img} alt={props.description.data.name}/>
-      <p>{props.description.data.name}</p>
-      <p>Genesis Date: {(props.description.data.genesis_date) ? props.description.data.genesis_date : 'N/A'} </p> 
-      <p>Market Cap Rank: {props.description.data.market_cap_rank}</p> 
-      <p>Circulating Supply: {props.description.circulatingSupply}</p> 
-      <p>Current Price: ${props.description.price}</p> 
-      <p>Price Change (24h): {props.description.priceChange}</p> 
-      <p>Analyst up sentiment: {props.description.data.sentiment_votes_up_percentage}% </p>
-      <p> Analyst down sentiment: {props.description.data.sentiment_votes_down_percentage}%</p>
+      <img src={props.details.img} alt={props.details.data.name}/>
+      <p>{props.details.data.name}</p>
+      <p>Genesis Date: {(props.details.data.genesis_date) ? props.details.data.genesis_date : 'N/A'} </p> 
+      <p>Market Cap Rank: {(props.details.data.market_cap_rank) ? props.details.data.market_cap_rank : "N/A" }</p> 
+      <p>Circulating Supply: {(props.details.circulatingSupply) ? props.details.circulatingSupply : "N/A"}</p> 
+      <p>Current Price: ${(props.details.price) ? props.details.price : "N/A"}</p> 
+      <p>Price Change (24h): {(props.details.priceChange) ? props.details.priceChange : "N/A"}</p> 
+      <p>Analyst up sentiment: {(props.details.data.sentiment_votes_up_percentage) ? props.details.data.sentiment_votes_up_percentage : "N/A"}% </p>
+      <p> Analyst down sentiment: {(props.details.data.sentiment_votes_down_percentage) ? props.details.data.sentiment_votes_down_percentage : "N/A"}%</p>
     </div>
   )
 }
