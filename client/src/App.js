a38873b3bcab510aa948201299e263924da8f7b0
Saurabh Dabas <developerdabas@gmail.com>
2022-04-17 12:18:21 +0530

    Add Header component

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import Dashboard from './components/Dashboard/Dashboard';
 import SideBarList from './components/SideBar/SideBarList';
 import WatchList from './components/WatchList';
 import SingleCrypto from './components/SingleCrypto/SingleCrypto';
+import Header from  './components/Header/Header'
 
 function App() {
   return (
     <>
+      <Header/>
       <SideBarList/>
       <Routes>
         <Route path="/dashboard" element={<Dashboard/>}/> 
