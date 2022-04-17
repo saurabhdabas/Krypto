44ee794dca47c2b9bf3b6a506f97454fd155e0a6
Saurabh Dabas <developerdabas@gmail.com>
2022-04-17 12:06:29 +0530

    Add a Chart Component to render chart for evry crypto

--- client/src/components/SingleCrypto/Chart.js
+++ client/src/components/SingleCrypto/Chart.js
@@ -1,11 +1,84 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { 
+  Chart as 
+  ChartJS,  
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+  } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-const Chart = () => {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Charts = () => {
+  const { id, day_id } = useParams();
+  console.log("day_id:",day_id);
+  const [chartData, setChartData] = useState([]);
+
+  const [day, setDay] = useState(1);
+
+  useEffect(() => {
+    axios.get(`/chart/${id}/days/${day}`) 
+      .then((res) => { 
+        setChartData(res.data.prices)
+        }
+      )
+      .catch((err)=>console.log(err));
+  },[id, day]);
+
+  const dayHandler = (day) => {
+    setDay(day);
+  }
+
+  const data = {
+    labels: chartData.map((crypto) => {
+          console.log("crypto:",chartData)
+          let date = new Date(crypto[0]);
+          let time = date.toLocaleDateString();
+          return day === 1 ? time : date.toLocaleDateString()
+    }),
+    datasets: [
+      {
+        data: chartData.map((crypto) => crypto[1]),
+        label: `${id.toUpperCase()} ( Past ${day} Days ) in CAD`,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)'
+      },
+    ]
+  }
+  const options={
+    elements: {
+      point: {
+        radius: 2
+      },
+      responsive:true
+    }
+  }
   return (
-    <div>
-      
-    </div>
+  <>
+    <Line data={data} options = {options}/>
+    <button onClick={() => dayHandler(1)}>One Day</button>
+    <button onClick={() => dayHandler(7)}>One Week</button>
+    <button onClick={() => dayHandler(30)}>One Month</button>
+    <button onClick={() => dayHandler(365)}>One Year</button>
+  </>
   )
 }
 
-export default Chart
+export default Charts
+
+
