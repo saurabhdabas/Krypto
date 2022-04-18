f46e738fce8d0ec2bcff33aa563e86f04632c340
Saurabh Dabas <developerdabas@gmail.com>
2022-04-19 04:48:25 +0530

    Remove Header and SideBar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
-import SideBarList from './components/SideBar/SideBarList';
+
 import WatchList from './components/WatchList';
 import SingleCrypto from './components/SingleCrypto/SingleCrypto';
-import Header from  './components/Header/Header'
+
 import ChatRooms from './components/Chat/ChatRooms';
 import Login from './components/Login';
+import LoginTest from './components/LoginTest';
 
 function App() {
   return (
     <>
-      <Header/>
-      <SideBarList/>
+
       <Routes>
         
-        <Route path="/login" element={<Login/>}/> 
+        <Route path="/" element={<Login/>} /> 
+        <Route path="/login" element={<LoginTest/>}/> 
         <Route path="/dashboard" element={<Dashboard/>}/> 
         <Route path="/watchlist" element={<WatchList/>}/>
         <Route path="/crypto/:id" element={<SingleCrypto/>}/>
