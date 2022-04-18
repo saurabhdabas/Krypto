3c08f08903a2cf81ff4f2985062d0a732fefd368
Saurabh Dabas <developerdabas@gmail.com>
2022-04-18 09:27:41 +0530

    Add Chat Components

---
+++ client/src/components/Chat/ChatRooms.js
@@ -0,0 +1,36 @@
+import { React, useState} from 'react';
+import Button from '@mui/material/Button';
+import ChatRoom from './ChatRoom';
+const ChatRooms = () => {
+  const [room, setRoom] = useState("");
+  const button = () => {
+    console.log('this is the value of room', room)
+    if (room === "") {
+      return (
+        <div>
+          <Button variant="contained" onClick={()=> {setRoom('trending')}}># Trending Crypto</Button>
+          <Button variant="contained" onClick={()=> {setRoom('investment')}}># Investments</Button>
+          <Button variant="contained"onClick={()=> {setRoom('general')}}># General</Button>
+          <Button variant="contained" onClick={()=> {setRoom('events')}}># Events</Button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Button variant="contained"onClick={()=> {setRoom('')}}>Leave Room</Button>
+          <ChatRoom roomId={room}/>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div>
+      
+      {button()}
+      
+    </div>
+  )
+}
+
+export default ChatRooms;
