28320d96f814779ffad1cb32137a3a97c22f47bf
Saurabh Dabas <developerdabas@gmail.com>
2022-04-18 10:49:36 +0530

    Add Login route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import WatchList from './components/WatchList';
 import SingleCrypto from './components/SingleCrypto/SingleCrypto';
 import Header from  './components/Header/Header'
 import ChatRooms from './components/Chat/ChatRooms';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Header/>
       <SideBarList/>
       <Routes>
+        
+        <Route path="/" element={<Login/>}/> 
         <Route path="/dashboard" element={<Dashboard/>}/> 
         <Route path="/watchlist" element={<WatchList/>}/>
         <Route path="/crypto/:id" element={<SingleCrypto/>}/>
