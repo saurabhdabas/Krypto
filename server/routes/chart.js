0bdf43d8d94aee7f0da2dc6c45e038d03ee5aadf
Saurabh Dabas <developerdabas@gmail.com>
2022-04-18 09:18:37 +0530

    Remove unnecesarcy console.logs

--- server/routes/chart.js
+++ server/routes/chart.js
@@ -7,7 +7,7 @@ module.exports = (db) => {
     const id = req.params.id;
     const day_id = req.params.day;
     axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=cad&days=${day_id}&interval=daily`).then((response) => {
-      console.log(response.data)
+      
       res.json(response.data);
     }).catch((error) => {
       console.error(error);
