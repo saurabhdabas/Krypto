5557d8fd5cdff6755dc7f8c615e833f73445752b
Saurabh Dabas <developerdabas@gmail.com>
2022-04-18 09:30:29 +0530

    add server side socket connection

--- server/app.js
+++ server/app.js
@@ -27,5 +27,30 @@ app.use('/market', marketRouter(db));
 app.use('/crypto', cryptoRouter(db));
 app.use('/chart', chartRouter(db));
 
+// Socket Connection 
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3002',
+    method: ['GET', 'POST']
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 module.exports = {app, server};
