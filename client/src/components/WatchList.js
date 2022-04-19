33b53502d7ad51342ac99dca043928202bdc2e5a
Saurabh Dabas <developerdabas@gmail.com>
2022-04-19 08:33:02 +0530

    Import Header and Navigation component

--- client/src/components/WatchList.js
+++ client/src/components/WatchList.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Header from './Header/Header';
+import Navigation from './Navigation/Navigation';
 const WatchList = () => {
   return (
     <div>
+      <Header/>
+      <Navigation/>
       Hi, I'm the WatchList Component. Coming Soon.
     </div>
   )
