74ae03dfcdacf2b8a6af4e1567f3edd3ee0ed5a3
Saurabh Dabas <developerdabas@gmail.com>
2022-04-19 13:36:07 +0530

    Bug Fix the query

--- server/app.js
+++ server/app.js
@@ -34,7 +34,8 @@ app.put('/user-data',(req, res) => {
     email:req.body.data.email,
     password:req.body.data.password
   };
-  db.query(`SELECT * FROM users WHERE email = $1`, [data.email])
+
+  db.query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [data.email,data.password])
   .then(response => res.send(response.rows[0]))
   .catch(e => console.error(e.stack))
 });
