9ab8f08852dc364524003d160fa751e8eaf447cf
Saurabh Dabas <developerdabas@gmail.com>
2022-04-19 11:39:29 +0530

    Add Favourite button to crypto

--- client/src/components/Dashboard/Crypto.js
+++ client/src/components/Dashboard/Crypto.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import url from '../../helpers/urlDecoder';
-const Crypto = (props) => {
+import Fab from '@mui/material/Fab';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { red } from '@mui/material/colors';
 
+const Crypto = (props) => {
+  // const color = red[500];
   const decodedURL = url(props.id);
   return (
       <li>
@@ -15,6 +19,7 @@ const Crypto = (props) => {
         </div>
         <div>{props.current_price}</div>
         <div>{props.last_updated}</div>
+        <Fab aria-label="like"><FavoriteIcon style={{ color: red[500] }}/></Fab>
       </li>
   )
 }
