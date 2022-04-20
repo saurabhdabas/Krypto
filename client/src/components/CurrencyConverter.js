b5e290a59468d629a98b75d67316e7811a7c66cd
Saurabh Dabas <developerdabas@gmail.com>
2022-04-20 21:33:56 +0530

    Bug Fix for Conversion Formula

--- client/src/components/CurrencyConverter.js
+++ client/src/components/CurrencyConverter.js
@@ -13,8 +13,14 @@ import TextField from '@mui/material/TextField';
 
 export default function CurrencyConverter() {
   const [ state, setState] = useState([{data:[]}]);
-  const [ primary, setPrimary] = useState(null);
-  const [ secondary, setSecondary] = useState(null);
+  const [ primary, setPrimary] = useState({
+    price:"",
+    name:""
+  });
+  const [ secondary, setSecondary] = useState({
+    price:"",
+    name:""
+  });
   const [ number, setNumber] = useState(1);
   const [ result, setResult] = useState(0);
 
@@ -32,7 +38,7 @@ export default function CurrencyConverter() {
   const cryptoList = state[0].data.map((crypto)=>{
 
     return (
-      <MenuItem key ={crypto.name} value={crypto.current_price}>{crypto.name}<img src={crypto.image}alt = "crypto" width = '30' ></img></MenuItem>
+      <MenuItem key ={crypto.name} value={crypto}>{crypto.name}<img src={crypto.image}alt = "crypto" width = '30' ></img></MenuItem>
     );
   })
   const handleChange = (event) => {
@@ -43,22 +49,27 @@ export default function CurrencyConverter() {
   }
   const handleSecondary = (event) => {
     console.log("SecondaryValue:",event.target.value);
-    setSecondary(event.target.value);
+    setSecondary({...secondary,
+      price:event.target.value.current_price,
+      name:event.target.value.id
+    })
   };
   const handlePrimary = (event)=>{
     console.log("PrimaryValue:",event.target.value);
-    setPrimary(event.target.value)
+    console.log("PrimaryValue:",event.target.value.current_price);
+    setPrimary({...primary,
+      price:event.target.value.current_price,
+      name:event.target.value.id
+    })
   }
 
-  const amount = number * primary;
+  const amount = number * primary.price;
 
   const handleResult = () => {
     let final = 0;
-    if(amount > secondary){
-      final = amount/secondary;
-    } else {
-      final = secondary/amount;
-    }
+    
+    final = amount/secondary.price;
+    
     setResult(final);
   }
   console.log(result);
@@ -92,7 +103,7 @@ export default function CurrencyConverter() {
         <Select
           labelId="demo-simple-select-helper-label"
           id="demo-simple-select-helper"
-          value={primary ? primary : ""}
+          value={primary ? primary.price.current_price : ""}
           label="Primary"
           onChange={handlePrimary}
         >
@@ -108,7 +119,7 @@ export default function CurrencyConverter() {
         <Select
           labelId="demo-simple-select-helper-label"
           id="demo-simple-select-helper"
-          value={secondary ? secondary : ""}
+          value={secondary ? secondary.price.current_price : ""}
           label="Secondary"
           onChange={handleSecondary}
         >
@@ -128,7 +139,7 @@ export default function CurrencyConverter() {
         </Button>
       </div>
       <div>
-      {result ? result : null}
+      {result ? `${number}${primary.name}=${result}${secondary.name}` : null}
       </div>
     </Box>
   );
