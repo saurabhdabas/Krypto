288d26abc8d4dc6f95e9a35d5deb41a9eaac7386
Saurabh Dabas <developerdabas@gmail.com>
2022-04-21 03:17:49 +0530

    Add Circular Progress bar

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -7,20 +7,24 @@ import TrendingCryptoList from './TrendingCryptoList';
 import SearchForm from './SearchForm';
 import topFourTrending from '../../helpers/topFourTrending';
 import searchFilter from '../../helpers/searchFilter';
-
+import CircularProgress from '@mui/material/CircularProgress';
 const Dashboard = () => {
   const [state, setState] = useState([{
     trending:[],
     market:[]
   }]);
-  const [search, setSearch] = useState("");
+  const[search, setSearch] = useState("");
+  const[loading, setLoading] = useState(false)
   useEffect(() => {
     axios.get('/market') 
       .then((res) => {
         setState((prev)=>[{ ...prev,
           trending:topFourTrending(res.data),
           market:res.data}])
-        }
+        },
+        setTimeout(()=>{
+          setLoading(true)
+        }, 500)
       )
       .catch((err)=>console.log(err));
   },[]);
@@ -28,16 +32,21 @@ const Dashboard = () => {
     setSearch(event.target.value);
   };
   const filteredRows = searchFilter(state[0].market, search)
+  console.log("loading:",loading);
   return (
     <>
       <Header/>
       <Navigation/>
-      <TrendingCryptoList data={state[0].trending}/>
-      <SearchForm search={search} onChange={inputHandler}/>
-      <CryptoList data={filteredRows}/>
+
+      {loading ? 
+        (<div><TrendingCryptoList data={state[0].trending}/>
+       <SearchForm search={search} onChange={inputHandler}/>
+       <CryptoList data={filteredRows}/></div> )
+       : <CircularProgress/>}
     </>
   )
 }
 
 export default Dashboard
+
 
