72daf397a770dc8e5d5a982fee0234afc7569e16
Saurabh Dabas <developerdabas@gmail.com>
2022-04-21 13:14:57 +0530

    Fixed Schema for wishlists entity

--- server/db/schema/02_create_wishlists.sql
+++ server/db/schema/02_create_wishlists.sql
@@ -2,5 +2,8 @@ DROP TABLE IF EXISTS wishlists CASCADE;
 
 CREATE TABLE wishlists (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  user_email VARCHAR(255) NOT NULL,
+  crypto_id VARCHAR(255) NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  user_email_cyrpto_id VARCHAR(255) NOT NULL UNIQUE
 );
