cd286df756ac80d0eed1c0fbd7301066a8abaadd
Saurabh Dabas <developerdabas@gmail.com>
2022-04-21 13:09:43 +0530

    Bug Fix for Client request to send user favorites to server

--- client/src/components/Dashboard/Crypto.js
+++ client/src/components/Dashboard/Crypto.js
@@ -6,23 +6,23 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import { red } from '@mui/material/colors';
 
 
-const Crypto = (props) => {
-  
+const Crypto = (props) =>{
+
   const user = JSON.parse(localStorage.getItem('username'));
   const decodedURL = url(props.id);
   return (
-      <li>
-        <Link to={`/crypto/${decodedURL.toLowerCase()}`}>
-          <img src={props.image} alt={props.name}></img>
-        </Link>
-        <div>
-          <span>{props.name}</span>
-          <span>{props.price_change_percentage_24h}</span>
-        </div>
-        <div>{props.current_price}</div>
-        <div>{props.last_updated}</div>
-        <Fab aria-label="like" onClick={()=>{props.setFavorite({name:user.name,favorite:props.name})}}><FavoriteIcon  style={{ color: red[500] }}/></Fab>
-      </li>
+    <li>
+      <Link to={`/crypto/${decodedURL.toLowerCase()}`}>
+        <img src={props.image} alt={props.name}></img>
+      </Link>
+      <div>
+        <span>{props.name}</span>
+        <span>{props.price_change_percentage_24h}</span>
+      </div>
+      <div>{props.current_price}</div>
+      <div>{props.last_updated}</div>
+      <Fab aria-label="like" onClick={(prev)=>props.setFavorite(({...prev,user:user.name,fav:props.name}))}><FavoriteIcon  style={{ color: red[500] }}/></Fab>
+    </li>
   )
 }
 
