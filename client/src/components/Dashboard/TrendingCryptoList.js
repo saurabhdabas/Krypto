52ecb40897bdfdf4f7444fcdac65e93eab79f626
Saurabh Dabas <developerdabas@gmail.com>
2022-04-22 10:04:42 +0530

    Style List of Trending crypto component

--- client/src/components/Dashboard/TrendingCryptoList.js
+++ client/src/components/Dashboard/TrendingCryptoList.js
@@ -1,14 +1,14 @@
 import {React} from 'react';
-
+import { Grid } from "@mui/material";
 import TrendingCrypto from './TrendingCrypto';
 
-const TrendingCryptoList = (props) => {
+export default function TrendingCryptoList(props) {
 
-  const TrendingCryptoList = props.data.map((crypto)=>{
+  const trendingCrypto = props.data.map((crypto)=>{
     return (
       <TrendingCrypto
       key = {crypto.id}
-      id= {crypto.id}
+      id={crypto.id}
       image={crypto.image}
       name={crypto.name}
       price_change_percentage_24h={crypto.price_change_percentage_24h}
@@ -17,11 +17,38 @@ const TrendingCryptoList = (props) => {
       />
     );
   });
+
   return (
-    <div>
-      {TrendingCryptoList}
-    </div>
+    <Grid container direction={"row"}>
+      {trendingCrypto}
+    </Grid>
   )
 }
+// import {React} from 'react';
 
-export default TrendingCryptoList
+// import TrendingCrypto from './TrendingCrypto';
+// import Box from '@mui/material/Box';
+// const TrendingCryptoList = (props) => {
+
+//   const TrendingCryptoList = props.data.map((crypto)=>{
+//     return (
+
+//         <TrendingCrypto
+//         key = {crypto.id}
+//         id= {crypto.id}
+//         image={crypto.image}
+//         name={crypto.name}
+//         price_change_percentage_24h={crypto.price_change_percentage_24h}
+//         current_price={crypto.current_price}
+//         last_updated={crypto.last_updated}
+//         />
+//     );
+//   });
+//   return (
+//     <Box gridColumn="span">
+//       {TrendingCryptoList}
+//     </Box>
+//   )
+// }
+
+// export default TrendingCryptoList
