8e1b0cd7f0d5165ed325d7aa03149427d816cb63
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 07:25:20 +0530

    Add styling to form

--- client/src/components/Dashboard/SearchForm.js
+++ client/src/components/Dashboard/SearchForm.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import { Grid } from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 const SearchForm = (props) => {
-
+  const darkTheme = createTheme({
+    palette: {
+      mode: props.mode,
+    },
+  });
   return (
+    <ThemeProvider theme={darkTheme}>
+    <Grid container justifyContent="center">
     <form>
-      <input value={props.search} onChange={props.onChange} name="value" placeholder="Enter the crypto you want to search for">
+      <input style={{width: 400, height: 40 }} value={props.search} onChange={props.onChange} name="value" placeholder="Enter the crypto you want to search for">
       </input>
     </form>
+   
+    </Grid>
+    </ThemeProvider>
   )
 }
 
-export default SearchForm
+export default SearchForm;
+
+// import React from 'react'
+
+// const SearchForm = (props) => {
+
+//   return (
+//     <form>
+//       <input value={props.search} onChange={props.onChange} name="value" placeholder="Enter the crypto you want to search for">
+//       </input>
+//     </form>
+//   )
+// }
+
+// export default SearchForm
