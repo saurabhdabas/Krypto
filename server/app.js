b4c05df2bbb2b1a9758a522bb58955277cd754dc
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 10:46:11 +0530

    Bug fix for retrieving favorites

--- server/app.js
+++ server/app.js
@@ -40,14 +40,11 @@ app.put('/user-data',(req, res) => {
     .catch(e => console.error(e.stack));
 });
 
-
-
-
 //used to insert into watchlist
 app.put('/insert-watchlist',(req, res) => {
   let response = req.body.data;
   console.log("req.body for fav: " , req.body);
-  db.query(`INSERT INTO watchlists (user_email, crypto_id, image, user_email_cyrpto_id)
+  db.query(`INSERT INTO watchlists (user_email, crypto_id, img, user_email_crypto_id)
   VALUES ($1, $2, $3, $4) RETURNING *;`, [req.body.user.email, req.body.id, req.body.img,`${req.body.user.email}${req.body.id}`])
     .then((res) => res.send(res))
     .catch((error) => res.send(error));
@@ -68,7 +65,7 @@ app.put('/user-delete',(req, res) => {
 app.put('/watchlist',(req, res) => {
   let response = req.body.user.email;
   console.log("this is the response: " , response);
-  db.query(`select crypto_id, image FROM watchlists where user_email = $1`, [response])
+  db.query(`select crypto_id, img FROM watchlists where user_email = $1`, [response])
     .then(response => res.send(response.rows))
     .catch((error) => res.send(error));
 });
