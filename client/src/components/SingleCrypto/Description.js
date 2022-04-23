35c5f568f44ab908b27931a6670b0836556f2bd4
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 08:15:32 +0530

    Add more styling to the page

--- client/src/components/SingleCrypto/Description.js
+++ client/src/components/SingleCrypto/Description.js
@@ -1,25 +1,83 @@
 import React from 'react'
-import Fab from '@mui/material/Fab';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { red } from '@mui/material/colors';
+
+import { Grid, Paper } from '@mui/material';
+
 const Description = (props) => {
   
   return (
-    <div>
-      <img src={props.description.img} alt={props.description.data.name}/>
-      <p>{props.description.data.name}</p>
-      <p>Genesis Date: {(props.description.data.genesis_date) ? props.description.data.genesis_date : 'N/A'} </p> 
-      <p>Market Cap Rank: {(props.description.data.market_cap_rank) ? props.description.data.market_cap_rank : "N/A" }</p> 
-      <p>Circulating Supply: {(props.description.circulatingSupply) ? props.description.circulatingSupply : "N/A"}</p> 
-      <p>Current Price: ${(props.description.price) ? props.description.price : "N/A"}</p> 
-      <p>Price Change (24h): {(props.description.priceChange) ? props.description.priceChange : "N/A"}</p> 
-      <p>Analyst up sentiment: {(props.description.data.sentiment_votes_up_percentage) ? props.description.data.sentiment_votes_up_percentage : "N/A"}% </p>
-      <p> Analyst down sentiment: {(props.description.data.sentiment_votes_down_percentage) ? props.description.data.sentiment_votes_down_percentage : "N/A"}%</p>
-      <Fab  aria-label="like">
-        <FavoriteIcon style={{ color: red[500] }}/>
-      </Fab>
-    </div>
+  <div>
+    <Grid container  >
+      <Grid container direction={'row'} alignItems="center" justifyContent="center" m={2}>
+        <Paper sx={{
+          p: 1,
+          m:3,
+          backgroundColor: (theme) => theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)' }}>
+          <Grid item >
+          <p>Genesis Date: {(props.description.data.genesis_date) ? props.description.data.genesis_date : 'N/A'} </p> 
+          </Grid>
+        </Paper>
+        <Paper sx={{
+          p: 1,
+          m:3,
+          backgroundColor: (theme) => theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)' }}>
+          <Grid item>
+          <p>Market Cap Rank: {(props.description.data.market_cap_rank) ? props.description.data.market_cap_rank : "N/A" }</p> 
+          </Grid>
+        </Paper>
+        <Paper sx={{
+          p: 1,
+          m:3,
+          backgroundColor: (theme) => theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)' }}>
+          <Grid item>
+          <p>Current Price: ${(props.description.price) ? props.description.price : "N/A"}</p> 
+          </Grid>
+        </Paper>
+        <Paper sx={{
+          p: 1,
+          m:3,
+         
+          backgroundColor: (theme) => theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)' }}>
+            <Grid item>
+            <p>Price Change (24h): {(props.description.priceChange) ? props.description.priceChange : "N/A"}</p> 
+            </Grid>
+        </Paper>
+
+      </Grid>
+      
+      <Grid container direction={'row'} alignItems="center" justifyContent="center">
+      <Paper sx={{
+          p: 1,
+          m:3,
+          mt:-3,
+          backgroundColor: (theme) => theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)' }}>
+          <Grid item>
+          <p>Circulating Supply: {(props.description.circulatingSupply) ? props.description.circulatingSupply : "N/A"}</p> 
+          </Grid>
+        </Paper>
+        <Paper sx={{
+          p: 1,
+          m:3,
+          mt:-3,
+          backgroundColor: (theme) => theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)' }}>
+            <Grid item>
+            <p>Analyst up sentiment: {(props.description.data.sentiment_votes_up_percentage) ? props.description.data.sentiment_votes_up_percentage : "N/A"}% </p>
+            </Grid>
+        </Paper>
+        <Paper sx={{
+          p: 1,
+          m:3,
+          mt:-3,
+          backgroundColor: (theme) => theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)' }}>
+            <Grid item>
+            <p> Analyst down sentiment: {(props.description.data.sentiment_votes_down_percentage) ? props.description.data.sentiment_votes_down_percentage : "N/A"}%</p>
+            </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  </div>
+ 
   )
+  
 }
 
 export default Description
