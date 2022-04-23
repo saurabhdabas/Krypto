f27bcc63f2475a350ae73716ecf3e875a12b63b1
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 23:55:32 +0530

    Replace circular loader with a skeleton to show while loading

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -7,46 +7,81 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import {descriptionspliter, titlespliter} from '../../helpers/descriptionSpilter';
+import {descriptionspliter, titlespliter} from '../../helpers/descriptionSpliter';
 import { newsDateConvert } from '../../helpers/dateConvert';
+import Skeleton from '@mui/material/Skeleton';
 
 import Paper from '@mui/material/Paper';
 
 
-const News = ({title, image, description, date, source}) => {
+const News = ({title, image, description, date, source, loading}) => {
 
-  
-  return (
-  <Box m="auto">  
-        {/* <Item sx={{ gridRow: '1', gridColumn: 'span 2' }}> */}
-        <Paper elevation={3}> 
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="news"
-              height="140"
-              image={image}
-            />
-            <Divider variant="inset"/>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {newsDateConvert(date)}
-              </Typography>
-              <Typography gutterBottom variant="h5" component="div">
-                {titlespliter(title)}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {descriptionspliter(description)}
-              </Typography>
-            </CardContent>
-            <Divider variant="middle"/>
-            <CardActions>
-              <Button size="small" onClick = {(e) => (window.location.href = `${source}`)}>Source</Button>
-            </CardActions>
-          </Card>
-      </Paper>
-  </Box>
-  )
+  if (loading){
+    return (
+    <Box m="auto">  
+          {/* <Item sx={{ gridRow: '1', gridColumn: 'span 2' }}> */}
+          <Paper elevation={3}> 
+            <Card sx={{ maxWidth: 300, mb:4}}>
+              <CardMedia
+                component="img"
+                alt="news"
+                height="140"
+                image={image}
+              />
+              <Divider variant="inset"/>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {newsDateConvert(date)}
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  {titlespliter(title)}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {(description) ? descriptionspliter(description) : 
+                  <div ><Skeleton animation={false} />
+                  <Skeleton animation={false} />
+                  <Skeleton animation={false} /></div> }
+                </Typography>
+              </CardContent>
+              <Divider variant="middle"/>
+              <CardActions>
+                <Button size="small" onClick = {(e) => (window.location.href = `${source}`)}>Source</Button>
+              </CardActions>
+            </Card>
+        </Paper>
+    </Box>
+    )
+  } else {
+    return (
+      <Box m="auto">  
+      {/* <Item sx={{ gridRow: '1', gridColumn: 'span 2' }}> */}
+      <Paper elevation={3}> 
+        <Card sx={{ maxWidth: 300, mb:4}}>
+        <Skeleton variant="rectangular" animation="wave" width={280} height={140} />
+          <Divider variant="inset"/>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            <Skeleton animation="wave" />
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+            <Skeleton animation="wave"/>
+            <Skeleton animation="wave"/>
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <Skeleton animation="wave"/>
+              <Skeleton animation="wave"/>
+              <Skeleton animation="wave"/>
+            </Typography>
+          </CardContent>
+          <Divider variant="middle"/>
+          <CardActions>
+            <Button size="small" onClick = {(e) => (window.location.href = `${source}`)}>Source</Button>
+          </CardActions>
+        </Card>
+    </Paper>
+</Box>
+    )
+  }
 }
 
 export default News;
