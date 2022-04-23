d82c1e195035523a6d4448b5dbfec8483dbe7cb6
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 07:39:52 +0530

    Fix Styling issues and add some icons

--- client/src/components/Dashboard/Crypto.js
+++ client/src/components/Dashboard/Crypto.js
@@ -1,5 +1,6 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
+
 import url from '../../helpers/urlDecoder';
 
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -7,34 +8,83 @@ import { red } from '@mui/material/colors';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import { Button } from '@mui/material';
+import {useState, useEffect} from 'react';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import dateConvert from '../../helpers/dateConvert';
+import ClearIcon from '@mui/icons-material/Clear';
+
+import '../../index.css'
 
 const Crypto = (props) => {
-  let decodedURL = url(props.id);
+  const decodedURL = url(props.id);
+  const [textColor, setTextColor] = useState('black');
+  useEffect(() => {
+    if (props.mode === 'dark') {
+      setTextColor('white');
+    } else if (props.mode === 'light') {
+      setTextColor('black');
+    }
+  }, [props.mode])
+
   return (
-    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+
+
+    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 }}}>
     
       <TableCell component="th" scope="row">
-        <Link to={`/crypto/${decodedURL.toLowerCase()}`}><img src={props.image} alt={props.id} width="50"/></Link>
-      </TableCell>
-      <TableCell align="left">
-        <Link to={`/crypto/${decodedURL.toLowerCase()}`}>{props.name}</Link>
-      </TableCell>
-      <TableCell align="left">
-        <Link to={`/crypto/${decodedURL.toLowerCase()}`}>{props.current_price}</Link>
-      </TableCell>
-      <TableCell align="left">
-        <Link to={`/crypto/${decodedURL.toLowerCase()}`}>{props.price_change_percentage_24h}</Link>
-      </TableCell>
-      <TableCell align="left">
-        <Link to={`/crypto/${decodedURL.toLowerCase()}`}>{props.last_updated}</Link>
-      </TableCell>
-      <TableCell>
-        {<Button aria-label="like"  onClick={() => props.setFav([props.id, props.image])}>
-          <FavoriteIcon style={{ color: red[500] }}/>
-        </Button>}
+        <Link to={`/crypto/${decodedURL.toLowerCase()}`}>
+          <img src={props.image} alt={props.id} width="50"/>
+        </Link>
       </TableCell>
 
+      <TableCell align="left">
+        <Link to={`/crypto/${decodedURL.toLowerCase()}`} style={{ textDecoration: 'none', color: textColor}}>{props.name}
+        </Link>
+      </TableCell>
+
+      <TableCell align="left">
+        <Link to={`/crypto/${decodedURL.toLowerCase()}`} style={{ textDecoration: 'none', color: textColor}}>$ {(props.current_price)}
+        </Link>
+      </TableCell>
+
+      <TableCell align="left">
+        <Link to={`/crypto/${decodedURL.toLowerCase()}`} style={{ textDecoration: 'none', color: (Math.round(props.price_change_percentage_24h) > 0) ? "green" : "red"}}>
+          <div className='shiftdown'>
+            {(Math.round(props.price_change_percentage_24h) > 0) ? 
+            <div className='flexdown'>
+              <FileUploadIcon/>
+            </div> 
+            : <div className='flexdown'>
+              <FileDownloadIcon/>
+              </div>
+            }
+          </div>
+        </Link>
+      </TableCell>
+
+      <TableCell align="left">
+        <Link to={`/crypto/${decodedURL}`} style={{ textDecoration: 'none', color: textColor}}>
+          {dateConvert(props.last_updated)}
+        </Link>
+      </TableCell>
+
+      {(props.dashboard === "watchlist") ?
+        <TableCell>
+          <Button aria-label="like"  onClick={() => props.setDeleted(props.id)}>
+            <ClearIcon style={{ color: red[500] }}/>
+          </Button>
+        </TableCell>
+      :
+        <TableCell>
+          <Button aria-label="like"  onClick={() => props.setFav([props.id, props.image])}>
+            <FavoriteIcon style={{ color: red[500] }}/>
+          </Button>
+        </TableCell> 
+        }
+
     </TableRow>
+
   )
 }
 export default Crypto;
