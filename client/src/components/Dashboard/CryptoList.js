ab6fde9a77d9a67f7eca67a8229be1ee4274f5c3
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 07:57:55 +0530

    Add Styling to crypto list

--- client/src/components/Dashboard/CryptoList.js
+++ client/src/components/Dashboard/CryptoList.js
@@ -12,29 +12,85 @@ import MarketCryptoHeader from '../Header/MarketCryptoHeader';
 
 const CryptoList = (props) => {
   
-  const [favorite, setFavorite]= useState({
-    username:"",
-    email:"",
-    fav:"",
-    img:""
-  });
-  
-  const handleClick = () => {
-    if(favorite.username && favorite.fav ){
-      console.log("fav:",favorite);
-      axios.put('/user-fav',{data:{
-        username:favorite.username,
-        email:favorite.email,
-        fav:favorite.fav,
-        img:favorite.img
-      }})
-      .then((res) => console.log("This is the responds from /user-fav post: ", res)).catch((error) => console.log("from user-fav error: ", error))
+  const [num, setNum] = useState(0);
+
+  const [state, setState] = useState([{
+    watchlist: []
+  }]);
+
+  const [deleted, setDeleted] = useState("");
+  // const [render, setRender] = useState("");
+
+  const handleSubmitWatchlist = () => {
+    props.setRender(deleted);
+    if (deleted) {
+    axios.put(`/user-delete`, {data: deleted , user: JSON.parse(localStorage.getItem('username'))})
     }
   }
-  useEffect(()=>{
-    handleClick();
-    console.log("favorite has changed:",favorite)
-  },[favorite]);
+  useEffect(() => {
+    handleSubmitWatchlist();
+  }, [deleted]);
+  useEffect(() => {
+    Promise.all([
+      // axios.get('/market'),
+      axios.put('/watchlist', {user: JSON.parse(localStorage.getItem('username'))} )
+    ]).then((all)=> {
+      console.log('This is what is returned from the api calls:', all);
+      setState(prev => [{...prev,
+        watchlist: all[0].data
+      }])
+    })
+
+  },[props.render, props.dashboard]);
+  
+  const [fav, setFav] = useState([]);  
+
+  const handleSubmit = () => {
+    
+    if (fav.length > 0) {
+      console.log("axios call to the back end to store fav gets called")
+      axios.put(`/insert-watchlist`, {
+        id: fav[0], 
+        img: fav[1], 
+        user: JSON.parse(localStorage.getItem('username'))})
+        .then((res) => 
+        console.log("This is the responds from /insert-watchlist post: ", res))
+        .catch((error) => console.log("from insert-watchlist error: ", error))
+    }
+  }
+  
+  useEffect(() => {
+    handleSubmit();
+  }, [fav]);
+
+  const arr =[];
+  const filter = state[0].watchlist.map((marketCrypto) => {
+    props.data.map((crypto)=> {
+      if (marketCrypto.crypto_id === crypto.id) {
+        arr.push(crypto)
+      }
+    })
+  })
+
+  const watchlistCrypto = arr.map((crypto)=>{
+    return (
+      <Crypto
+      key={crypto.id}
+      id={crypto.id}
+      image={crypto.image}
+      name={crypto.name}
+      price_change_percentage_24h={crypto.price_change_percentage_24h}
+      current_price={crypto.current_price}
+      last_updated={crypto.last_updated}
+      setFav={setFav}
+      fav={fav}
+      mode={props.mode} 
+      setMode={props.setMode}
+      dashboard={props.dashboard}
+      setDeleted={setDeleted}
+      />
+    );
+  });
 
   const CoinsList = props.data.map((crypto)=>{
     return (
@@ -46,23 +102,38 @@ const CryptoList = (props) => {
       price_change_percentage_24h={crypto.price_change_percentage_24h}
       current_price={crypto.current_price}
       last_updated={crypto.last_updated}
-      setFavorite = {setFavorite}
-      favorite={favorite}
+      setFavorite = {setFav}
+      favorite={fav}
+      mode={props.mode} 
+      setMode={props.setMode}
+      dashboard={props.dashboard}
+      setDeleted={setDeleted}
+      setNum={setNum}
+      num={num}
       />
     );
   });
   return (
-  <Grid container direction={"column"} style={{maxHeight: '50vh', overflow: 'hidden' }}>      
-            
-    <TableContainer component={Paper}>
-      <Table stickyHeader aria-label="sticky table" align="left">
-        <MarketCryptoHeader/>
-        <TableBody>  
-            {CoinsList}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </Grid>
+<div >
+            <Grid container direction={"column"} style={{maxHeight: '52.5vh', overflow: 'hidden'}} mb={2.8} 
+           >         
+            <TableContainer component={Paper}  sx= {
+              {backgroundColor: (theme) =>
+              theme.palette.mode === 'dark' ? 'grey' : 'rgb(238, 238, 238)', borderTop: "1px solid grey", borderBottom: "1px solid grey",
+              mb: 4
+              }
+            }>
+            <Table stickyHeader aria-label="sticky table" align="left">
+              <MarketCryptoHeader dashboard={props.dashboard}/>
+                    <TableBody>  
+                        {(props.dashboard === "market") ? 
+                        CoinsList : watchlistCrypto}
+                    </TableBody>
+            </Table>
+          </TableContainer>
+       
+        </Grid>
+      </div>
   )
 };
 
