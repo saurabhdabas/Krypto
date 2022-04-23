acd52562c2a8468f32aa9d7bd54128dcde0c4446
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 08:33:38 +0530

    Set Dark and Light mode and remove watchlist route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-
+import {useState} from 'react';
 import Dashboard from './components/Dashboard/Dashboard';
 
-import Wishlist from './components/Favorites/Wishlist';
+
 import SingleCrypto from './components/SingleCrypto/SingleCrypto';
 
 import ChatRooms from './components/Chat/ChatRooms';
@@ -13,36 +13,30 @@ import ProtectedRoutes from './components/Hooks/userAuth';
 
 import CurrencyConverter from './components/CurrencyConverter';
 
-function App() {
-
+function App(){
+  const [mode, setMode] = useState("light")
   return (
-    
-
+    <div className={mode}>
       <Routes>
 
           <Route path="/" element={<Login/>}/> 
 
           <Route path='/' element={<ProtectedRoutes/>}>
-            <Route path="/dashboard" element={<Dashboard/>}/> 
-          
-          
-            <Route path="/watchlist" element={<Wishlist/>}/>
-          
-            <Route path="/news" element={<NewsList/>}/>
+            <Route path="/dashboard" element={<Dashboard mode={mode} setMode={setMode}/>}/>         
+                    
+            <Route path="/news" element={<NewsList mode={mode} setMode={setMode}/>}/>
 
           
-            <Route path="/crypto/:id" element={<SingleCrypto/>}/>
+            <Route path="/crypto/:id" element={<SingleCrypto mode={mode} setMode={setMode}/>}/>
           
           
-            <Route path="/chatrooms" element={<ChatRooms/>}/>
+            <Route path="/chatrooms" element={<ChatRooms mode={mode} setMode={setMode}/>}/>
 
-            <Route path="/calculators" element={<CurrencyConverter/>}/>
+            <Route path="/calculators" element={<CurrencyConverter mode={mode} setMode={setMode}/>}/>
           </Route>
       
-      </Routes>
-      
-    
-
+      </Routes>    
+    </div>
   );
 }
 
