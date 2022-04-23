b52ae20bc66158dff08892d065cdcc3740b4d3cd
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 08:40:23 +0530

    Add routes to retrieve and delete user favorites

--- server/app.js
+++ server/app.js
@@ -36,35 +36,42 @@ app.put('/user-data',(req, res) => {
   };
 
   db.query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [data.email,data.password])
-  .then(response => res.send(response.rows[0]))
-  .catch(e => console.error(e.stack))
+    .then(response => res.send(response.rows[0]))
+    .catch(e => console.error(e.stack));
 });
-// Add Favourites to wishlist/watchlist
-app.put('/user-fav',(req, res) => {
-  
-  let response = req.body.data;
 
-  db.query(`INSERT INTO wishlists (username,email,crypto_id,img,email_crypto_id)
-  VALUES ($1, $2, $3, $4, $5) RETURNING *;`,[req.body.data.username, req.body.data.email, req.body.data.fav,req.body.data.img,`${req.body.data.email}${req.body.data.fav}`])
+
+
+
+//used to insert into watchlist
+app.put('/insert-watchlist',(req, res) => {
+  let response = req.body.data;
+  console.log("req.body for fav: " , req.body);
+  db.query(`INSERT INTO watchlists (user_email, crypto_id, image, user_email_cyrpto_id)
+  VALUES ($1, $2, $3, $4) RETURNING *;`, [req.body.user.email, req.body.id, req.body.img,`${req.body.user.email}${req.body.id}`])
     .then((res) => res.send(res))
     .catch((error) => res.send(error));
 });
-// //Retrive all the favorites from the wishlist
-// app.put('/fav-list',(req, res) => {
-//   let response = req.body.user.email;
-//   console.log("this is the response: " , response);
-//   db.query(`select crypto_id, image FROM wishlists where user_email = $1`, [response])
-//     .then(response => res.send(response.rows))
-//     .catch((error) => res.send(error));
-// });
-// //Delete Favorites from wishlist
-// app.put('/user-delete',(req, res) => {
-//   let response = req.body.data;
-//   console.log("req.body for fav: " , req.body);
-//   db.query(`DELETE FROM wishlists WHERE user_email = $1 and crypto_id = $2;`, [req.body.user.email,req.body.data])
-//     .then((res) => res.send(res))
-//     .catch((error) => res.send(error));
-// });
+
+//used to delete from watchlist
+app.put('/user-delete',(req, res) => {
+  let response = req.body.data;
+  console.log("req.body for fav: " , req.body);
+  db.query(`DELETE FROM watchlists WHERE user_email = $1 and crypto_id = $2;`, [req.body.user.email,req.body.data])
+    .then((res) => res.send(res))
+    .catch((error) => res.send(error));
+});
+
+
+
+//used to retrive all the favs from the watchlist database
+app.put('/watchlist',(req, res) => {
+  let response = req.body.user.email;
+  console.log("this is the response: " , response);
+  db.query(`select crypto_id, image FROM watchlists where user_email = $1`, [response])
+    .then(response => res.send(response.rows))
+    .catch((error) => res.send(error));
+});
 
 
 
