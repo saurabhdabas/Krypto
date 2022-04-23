8e0394180cd93caf2286c86d0f68c85a3f082e06
Saurabh Dabas <developerdabas@gmail.com>
2022-04-23 07:20:23 +0530

    Add a helper fucntion to convert date format

---
+++ client/src/helpers/dateConvert.js
@@ -0,0 +1,17 @@
+const dateConvert = (data) => {
+  let string = "";
+  
+  for (let i = 0; i < data.length; i++){
+    if (data[i] === "T"){
+      string += " "
+    }
+    else if (data[i] === '.'){
+      return string + ' GMT'
+    }
+    else{string += data[i]};
+
+  }
+
+}
+
+export default dateConvert;
