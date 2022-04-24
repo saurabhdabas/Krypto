ac3a92c87de4ccbc633a093efac69b409161e336
Saurabh Dabas <developerdabas@gmail.com>
2022-04-24 09:36:55 +0530

    Add more styling

--- client/src/components/Chat/ChannelList.js
+++ client/src/components/Chat/ChannelList.js
@@ -6,10 +6,11 @@ export class ChannelList extends React.Component {
 
     handleClick = id => {
         this.props.onSelectChannel(id);
+        
     }
-
+    
     render() {
-
+        console.log("propsChannelList:",this.props);
         let list = <div >There is no channels to show</div>;
         if (this.props.channels && this.props.channels.map) {
             list = this.props.channels.map(c => <Channel key={c.id} id={c.id} name={c.name} img={c.img} dis={c.dis} participants={c.participants} onClick={this.handleClick} />);
