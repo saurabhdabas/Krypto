211961cf3932452fbc6f20535d293b8115b6b45a
Saurabh Dabas <developerdabas@gmail.com>
2022-04-24 10:16:12 +0530

    Add Leave Room button to test

--- client/src/components/Chat/ChannelList.js
+++ client/src/components/Chat/ChannelList.js
@@ -8,12 +8,16 @@ export class ChannelList extends React.Component {
         this.props.onSelectChannel(id);
         
     }
+
+    handleLeave = participants => {
+        this.props.onLeaveChannel(participants);
+    }
     
     render() {
         console.log("propsChannelList:",this.props);
         let list = <div >There is no channels to show</div>;
         if (this.props.channels && this.props.channels.map) {
-            list = this.props.channels.map(c => <Channel key={c.id} id={c.id} name={c.name} img={c.img} dis={c.dis} participants={c.participants} onClick={this.handleClick} />);
+            list = this.props.channels.map(c => <Channel key={c.id} id={c.id} name={c.name} img={c.img} dis={c.dis} participants={c.participants} onClick={this.handleClick} onLeave ={this.handleLeave}/>);
         }
         return (
             <div>
