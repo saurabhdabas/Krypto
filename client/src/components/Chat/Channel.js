dd22b7a0f542167ed6ca4e870f5304ddb4dfa40f
Saurabh Dabas <developerdabas@gmail.com>
2022-04-24 07:37:59 +0530

    Add Channels for Chat room

---
+++ client/src/components/Chat/Channel.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import { Card } from '@mui/material';
+import { Typography } from '@mui/material';
+import { CardActions } from '@mui/material';
+import { Grid } from '@mui/material';
+
+export class Channel extends React.Component {
+
+  click = () => {
+    this.props.onClick(this.props.id);
+  }
+
+  render() {
+    return (
+      <Grid item align="center" xs={6}>
+        <Card sx={{ maxWidth: 250 , height: 325, mb: 3 } } align="center">
+          <Typography fontSize = {20}>{this.props.name} </Typography>
+          <img src={this.props.img} alt={this.props.name} width={300}/>
+          <CardActions align="center">
+            <div>
+              <Typography variant="body2" color="text.secondary">
+              {this.props.dis}
+              </Typography>
+              Total Users : {this.props.participants}
+              <div>
+                <Button size="medium" variant="contained" onClick={this.click} >Join Room</Button>
+              </div>
+             </div>
+          </CardActions>
+        </Card>
+      </Grid>    
+    );
+  }
+};
