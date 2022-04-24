faba372ef998f86d93e90839e9727ebcacebae5f
Saurabh Dabas <developerdabas@gmail.com>
2022-04-24 12:19:47 +0530

    Add styled badge to avatar

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -32,6 +32,7 @@ import {useEffect} from 'react';
 import { Avatar } from '@mui/material';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Badge from '@mui/material/Badge';
 
 const drawerWidth = 195;
 
@@ -101,6 +102,34 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
 
 
 export default function Navigation(props) {
@@ -159,7 +188,7 @@ export default function Navigation(props) {
     //   },
     // }));
 
-
+   
     const theme = useTheme();
     const [open, setOpen] = useState(false);
   
@@ -225,10 +254,17 @@ export default function Navigation(props) {
       </AppBar>
       <Drawer variant="permanent" open={open}>
         <DrawerHeader>
-        <Avatar alt="Remy Sharp"
-                    src={username.img}
-                    sx={{ width: 50, height: 50, border: 1, borderColor: "black" }}
-                  />
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            variant="dot"
+            width="5"
+          >
+            <Avatar alt="Remy Sharp"
+              src={username.img}
+              sx={{ width: 50, height: 50, border: 1, borderColor: "black" }}
+            />
+          </StyledBadge>
           <Typography variant="h6" noWrap component="div" pl={2} pt={1}>
             {username.name}   
           </Typography>
