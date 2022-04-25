6c5a0692bff47f08545e1f60105531dc7ecb39f5
Saurabh Dabas <developerdabas@gmail.com>
2022-04-25 07:21:21 +0530

    Set the Dark mode and light mode colours in palette

--- client/src/App.js
+++ client/src/App.js
@@ -12,31 +12,112 @@ import NewsList from './components/News/NewsList';
 import ProtectedRoutes from './components/Hooks/userAuth';
 
 import CurrencyConverter from './components/CurrencyConverter';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { grey,common} from '@mui/material/colors';
 
-function App(){
+
+function App() {
   const [mode, setMode] = useState("light")
+
+  const darkTheme = createTheme({
+    palette: {
+      ...(mode === 'light'
+      ? {
+          // palette values for light mode
+          primary: {  main: "#f5eeee" },
+          background: {
+            default: "#f5eeee",
+            paper: "#C5C0CD",
+          },
+          divider: common.black,
+          text: {
+            primary: grey[900],
+            secondary: grey[800],
+          },
+        }
+      : {
+          // palette values for dark mode
+          primary: {main: "#07060D"},
+          divider: common.white,
+          background: {
+            default: "#48374D",
+            paper: "rgb(60, 35, 60)",
+          },
+          text: {
+            primary: '#fff',
+            secondary: grey[100],
+          },
+        }),
+  },
+});
   return (
+    <ThemeProvider theme={darkTheme}>
     <div className={mode}>
       <Routes>
-
-          <Route path="/" element={<Login/>}/> 
-
-          <Route path='/' element={<ProtectedRoutes/>}>
-            <Route path="/dashboard" element={<Dashboard mode={mode} setMode={setMode}/>}/>         
-                    
-            <Route path="/news" element={<NewsList mode={mode} setMode={setMode}/>}/>
-
           
-            <Route path="/crypto/:id" element={<SingleCrypto mode={mode} setMode={setMode}/>}/>
-          
-          
-            <Route path="/chatrooms" element={<Chat mode={mode} setMode={setMode}/>}/>
+        <Route path="/" 
+          element={
+            <Login/>
+          }
+        /> 
 
-            <Route path="/calculators" element={<CurrencyConverter mode={mode} setMode={setMode}/>}/>
-          </Route>
-      
-      </Routes>    
+        <Route path='/' 
+          element={
+            <ProtectedRoutes/>
+          }
+        >
+
+        <Route path="/dashboard" 
+          element={
+            <Dashboard 
+              mode={mode} 
+              setMode={setMode}
+            />
+          }
+        /> 
+
+        <Route path="/news" 
+          element={
+            <NewsList 
+              mode={mode} 
+              setMode={setMode}
+            />
+          }
+        />
+
+        <Route path="/crypto/:id" 
+          element={
+            <SingleCrypto 
+              mode={mode} 
+              setMode={setMode}
+            />
+          }
+        />   
+
+        <Route path="/chatrooms" 
+          element={
+            <Chat 
+              mode={mode} 
+              setMode={setMode}
+            />
+          }
+        />
+
+        <Route path="/calculators" 
+          element={
+            <CurrencyConverter 
+              mode={mode} 
+              setMode={setMode}
+            />
+          }
+        />
+        
+        </Route>
+
+      </Routes>
+
     </div>
+</ThemeProvider>
   );
 }
 
