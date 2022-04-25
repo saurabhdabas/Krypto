47c3b6df3bc7c448d7f6d20db430f4a139e826e0
Saurabh Dabas <developerdabas@gmail.com>
2022-04-25 08:43:24 +0530

    Increase the Data received to 200 crypto from the API URL

--- server/routes/market.js
+++ server/routes/market.js
@@ -4,7 +4,7 @@ module.exports = (db) => {
   // all routes will go here 
 
   router.get('/', (req, res) => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false').then((response) => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=200&page=1&sparkline=false').then((response) => {
       res.json(response.data);
     }).catch((error) => {
       console.error(error);
