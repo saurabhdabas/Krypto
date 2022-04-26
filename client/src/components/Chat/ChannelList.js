44767b9398b3b73a4c83a28e9b597dc6d6c0fa7e
Saurabh Dabas <developerdabas@gmail.com>
2022-04-26 08:19:17 +0530

    Fix styling

--- client/src/components/Chat/ChannelList.js
+++ client/src/components/Chat/ChannelList.js
@@ -3,30 +3,24 @@ import { Channel } from './Channel';
 import { Grid } from '@mui/material';
 
 export class ChannelList extends React.Component {
-    
-    handleClick = (id) => {
-       console.log("this.props:",this.props);
+
+    handleClick = id => {
         this.props.onSelectChannel(id);
     }
 
-    // handleLeave = (id,participants) => {
-    //     console.log("this.props:",this.props);
-    //     console.log(participants);
-    //     this.props.onLeaveChannel(id,participants);
-    // }
-    
     render() {
-        console.log("propsChannelList:",this.props);
+
         let list = <div >There is no channels to show</div>;
         if (this.props.channels && this.props.channels.map) {
-            list = this.props.channels.map(c => <Channel key={c.id} id={c.id} name={c.name} img={c.img} dis={c.dis} participants={c.participants} onClick={this.handleClick}/>);
+            list = this.props.channels.map(c => <Channel key={c.id} id={c.id} name={c.name} img={c.img} dis={c.dis} participants={c.participants} onClick={this.handleClick} />);
         }
         return (
             <div>
-               <Grid container>
+               <Grid container> 
                     {list}
                 </Grid>
             </div>
-        );
-    };
-};
+            );
+    }
+
+}
