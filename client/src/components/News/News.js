0b3d8b213650e0a626ce6d89c36649d3b699305f
Saurabh Dabas <developerdabas@gmail.com>
2022-04-26 07:40:20 +0530

    Change the news link to open in a separate tab instead of same tab

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,90 +1,3 @@
-// import { React } from 'react'
-// import { Box } from '@mui/system';
-// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-// import Divider from '@mui/material/Divider';
-// import {descriptionspliter, titlespliter} from '../../helpers/descriptionSpliter';
-// import { newsDateConvert } from '../../helpers/dateConvert';
-// import Skeleton from '@mui/material/Skeleton';
-
-// import Paper from '@mui/material/Paper';
-
-
-// const News = ({title, image, description, date, source, loading}) => {
-
-//   if (loading){
-//     return (
-//     <Box m="auto">  
-//           {/* <Item sx={{ gridRow: '1', gridColumn: 'span 2' }}> */}
-//           <Paper elevation={3}> 
-//             <Card sx={{ maxWidth: 300, mb:4}}>
-//               <CardMedia
-//                 component="img"
-//                 alt="news"
-//                 height="140"
-//                 image={image}
-//               />
-//               <Divider variant="inset"/>
-//               <CardContent>
-//                 <Typography gutterBottom variant="h5" component="div">
-//                   {newsDateConvert(date)}
-//                 </Typography>
-//                 <Typography gutterBottom variant="h5" component="div">
-//                   {titlespliter(title)}
-//                 </Typography>
-//                 <Typography variant="body2" color="text.secondary">
-//                   {(description) ? descriptionspliter(description) : 
-//                   <div ><Skeleton animation={false} />
-//                   <Skeleton animation={false} />
-//                   <Skeleton animation={false} /></div> }
-//                 </Typography>
-//               </CardContent>
-//               <Divider variant="middle"/>
-//               <CardActions>
-//                 <Button size="small" onClick = {(e) => (window.location.href = `${source}`)}>Source</Button>
-//               </CardActions>
-//             </Card>
-//         </Paper>
-//     </Box>
-//     )
-//   } else {
-//     return (
-//       <Box m="auto">  
-//       {/* <Item sx={{ gridRow: '1', gridColumn: 'span 2' }}> */}
-//       <Paper elevation={3}> 
-//         <Card sx={{ maxWidth: 300, mb:4}}>
-//         <Skeleton variant="rectangular" animation="wave" width={280} height={140} />
-//           <Divider variant="inset"/>
-//           <CardContent>
-//             <Typography gutterBottom variant="h5" component="div">
-//             <Skeleton animation="wave" />
-//             </Typography>
-//             <Typography gutterBottom variant="h5" component="div">
-//             <Skeleton animation="wave"/>
-//             <Skeleton animation="wave"/>
-//             </Typography>
-//             <Typography variant="body2" color="text.secondary">
-//               <Skeleton animation="wave"/>
-//               <Skeleton animation="wave"/>
-//               <Skeleton animation="wave"/>
-//             </Typography>
-//           </CardContent>
-//           <Divider variant="middle"/>
-//           <CardActions>
-//             <Button size="small" onClick = {(e) => (window.location.href = `${source}`)}>Source</Button>
-//           </CardActions>
-//         </Card>
-//     </Paper>
-// </Box>
-//     )
-//   }
-// }
-
-// export default News;
 import { React } from 'react'
 import { Box } from '@mui/system';
 import Card from '@mui/material/Card';
@@ -99,7 +12,7 @@ const News = ({title, image, description, date, source, loading}) =>{
 
   if (loading){
     return (
-    <Box m="auto" onClick = {(e) => (window.location.href = `${source}`)}>
+    <Box m="auto" onClick = {(e) => (window.open(`${source}`))}>
           <Paper elevation={3}> 
             <Card sx={{ maxWidth: 300, mb:4}}>
               <CardMedia
@@ -123,7 +36,6 @@ const News = ({title, image, description, date, source, loading}) =>{
                   <Skeleton animation={false} /></div> }
                 </Typography>
               </CardContent>
-
             </Card>
         </Paper>
     </Box>
@@ -131,7 +43,6 @@ const News = ({title, image, description, date, source, loading}) =>{
   } else {
     return (
       <Box m="auto">  
-
       <Paper elevation={3}> 
         <Card sx={{ maxWidth: 300, mb:4}}>
         <Skeleton variant="rectangular" animation="wave" width={300} height={140} />
