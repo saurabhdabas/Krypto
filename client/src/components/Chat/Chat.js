688eea4b1a9078bd228b650e4968719e096fb9d4
Saurabh Dabas <developerdabas@gmail.com>
2022-04-26 08:28:24 +0530

    CHange the component styling structure for better page layout

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,12 +3,21 @@ import { ChannelList } from './ChannelList';
 import { MessagesPanel } from './MessagesPanel';
 import socketClient from "socket.io-client";
 import Navigation from '../Navigation/Navigation';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Grid } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import SendIcon from '@mui/icons-material/Send';
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Stack } from '@mui/material';
+import './Chat.scss'
+
 
 const SERVER = "http://127.0.0.1:8081";
 export class Chat extends React.Component {
-    
+
     state = {
         channels: null,
         socket: null,
@@ -21,7 +30,7 @@ export class Chat extends React.Component {
     }
 
     configureSocket = () => {
-        const socket = socketClient(SERVER);
+        var socket = socketClient(SERVER);
         socket.on('connection', () => {
             if (this.state.channel) {
                 this.handleChannelSelect(this.state.channel.id);
@@ -37,9 +46,8 @@ export class Chat extends React.Component {
             });
             this.setState({ channels });
         });
-
         socket.on('message', message => {
-            
+            console.log('this is the value of message: ', message)
             let channels = this.state.channels
             channels.forEach(c => {
                 if (c.id === message.channel_id) {
@@ -63,17 +71,15 @@ export class Chat extends React.Component {
     }
 
     handleChannelSelect = id => {
-        console.log("thisFromChat.js:",this);
         let channel = this.state.channels.find(c => {
-            console.log("c",c);
             return c.id === id;
         });
         this.setState({ channel });
         this.socket.emit('channel-join', id, ack => {
         });
     }
-    
-    
+
+
     handleSendMessage = (channel_id, text) => {
         const user=JSON.parse(localStorage.getItem('username'));
         const date = new Date();
@@ -86,28 +92,49 @@ export class Chat extends React.Component {
         this.socket.emit('send-message', { channel_id, text, senderName: this.socket.id, user: user.name, img: user.img , time: time, id: Date.now() });
     }
 
-    // darkTheme = 
-    //     createTheme({
-    //     palette: {
-    //     mode: this.props.mode,
-    //     },
-    //   });
+    
     render() {
 
         return (
-
-        <ThemeProvider theme={this.darkTheme}>
+        <div>
             <Navigation mode={this.props.mode} setMode={this.props.setMode}/>
-            <Grid  container direction={"row"}  ml={2} spacing={2} columns={12}>
-                <Grid item xs={6} >
-                    <ChannelList channels={this.state.channels} onSelectChannel={this.handleChannelSelect} onLeaveChannel={this.handleChannelLeave}/>
+            <Grid  container direction={"row"}  ml={4} spacing={2} columns={12}>
+                <Grid item xs={6}>
+                    <ChannelList channels={this.state.channels} onSelectChannel={this.handleChannelSelect} />
                 </Grid>
-                <Grid item xs={5}>
-                    <MessagesPanel onSendMessage={this.handleSendMessage} channel={this.state.channel} />
+                <Grid item xs={5} >
+                <Accordion disableGutters={true}>
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                      sx={{
+                        "&.Mui-expanded": {
+                          minHeight: 0
+                        },
+                        "& .MuiAccordionSummary-content.Mui-expanded": {
+                          margin: "5px 0"
+                        }
+                      }}
+                    >
+                      <Stack direction="row" spacing={2} alignItems="center">
+                        <InfoIcon style={{color:'red'}}/>
+                        <Typography component="h1" variant="h6" align='center'>
+                            Chat Room Guidelines
+                        </Typography>
+                      </Stack>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                      Do not verbally abuse, attack, embarrass, or threaten anyone else in the chat room, no matter
+                      what they might say to you.
+                      </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <MessagesPanel onSendMessage={this.handleSendMessage} channel={this.state.channel} />
                 </Grid>
             </Grid>
-        </ThemeProvider>
-
+        </div>
         );
     }
-};
+}
